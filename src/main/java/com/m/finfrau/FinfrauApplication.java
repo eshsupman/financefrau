6f0940452b3e1cc027ffe9608f4b73b91da015c4
eshsupman <Eshsupman@gmail.com>
2025-05-26 14:37:57 +0300

    init commit

---
+++ src/main/java/com/m/finfrau/FinfrauApplication.java
@@ -0,0 +1,13 @@
+package com.m.finfrau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinfrauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinfrauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/m/finfrau/FinfrauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m.finfrau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinfrauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
